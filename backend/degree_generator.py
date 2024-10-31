da8506c9fb7fa03b9dad1472885f84b81130c3d5
IanVictorSXA <91403350+IanVictorSXA@users.noreply.github.com>
2024-10-31 11:00:34 -0500

    semester_class and generator updated

---
+++ backend/degree_generator.py
@@ -0,0 +1,6 @@
+from semester_class import Semester
+
+class generator(object):
+
+    def __init__(self, time_to_graduate, poset, major_electives_credits):
+        self.semesters = [Semester() for i in range(time_to_graduate*3)]
--- backend/semester_class.py
+++ backend/semester_class.py
@@ -3,12 +3,17 @@
          
 class Semester:
     semester_names = ["Fall", 'Spring', 'Summer']
-    
+    index = 0
+    year = 2024
     def  __init__(self, credit_hours=15, max_credit_hours=19, coursesUser=[]):
-        self.credit_hours = credit_hours
         self.max_credit_hours = max_credit_hours
         self.coursesUser = coursesUser
         self.courses = []
+
+        self.semester_name = Semester.semester_names[Semester.index] + " " + str(Semester.year)
+        Semester.index = (Semester.index + 1)%3
+        if Semester.index == 1: Semester.year += 1
+        self.credit_hours = credit_hours if "Summer" not in self.semester_name else self.credit_hours = 0
 
     def add_node(self, node):
         
@@ -28,6 +33,7 @@ class Semester:
 
         self.courses.remove(node)
 
+semester = Semester()
     
 
     
